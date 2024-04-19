2d4d7699edc489195362b400e525be1410981c87
Sxlly <shaesullivan.buisness1@gmail.com>
2024-04-19 15:49:41 +0800

    pretty errors config

--- SpyApp.py
+++ SpyApp.py
@@ -41,8 +41,8 @@ pretty_errors.configure(
     separator_character='*',
     filename_display=pretty_errors.FILENAME_FULL,
     line_color=pretty_errors.RED + '>' + pretty_errors.default_config.line_color,
-    lines_before=5
-
+    lines_before=5,
+    lines_after=5
 )
 
 
